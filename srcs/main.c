47524ec19d4314d10f5052a7c710d96895ce38a7
imedgar <imedgar@student.21-school.ru>
2020-11-08 20:14:35 +0300

    ADD simple print env massive with printf

--- srcs/main.c
+++ srcs/main.c
@@ -6,16 +6,26 @@
 /*   By: imedgar <imedgar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/08 19:24:18 by imedgar           #+#    #+#             */
-/*   Updated: 2020/11/08 19:40:20 by imedgar          ###   ########.fr       */
+/*   Updated: 2020/11/08 20:13:56 by imedgar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
+**	TODO Delete stdio.h
+*/
+#include <stdio.h>
+
 int		main(int argc, char *argv[], char *env[])
 {
+	int i;
+
+	i = -1;
 	if (argc || argv || env)
 	{
+		while (env[++i])
+			printf("%s\n", env[i]);
 	}
 	return (0);
 }
