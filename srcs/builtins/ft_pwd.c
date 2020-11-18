e63f69e3f4c37cbb72bc16bf33d7983bae4e8923
imedgar <imedgar@student.21-school.ru>
2020-11-18 22:50:04 +0300

    DONE ft_pwd.c complete

--- srcs/builtins/ft_pwd.c
+++ srcs/builtins/ft_pwd.c
@@ -6,30 +6,31 @@
 /*   By: imedgar <imedgar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 10:16:40 by imedgar           #+#    #+#             */
-/*   Updated: 2020/11/16 21:31:17 by imedgar          ###   ########.fr       */
+/*   Updated: 2020/11/18 22:49:32 by imedgar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /*
-**			Our "pwd" takes 1 argument:
-**			1) int fd = filedescriptor to write
-**
-**			TODO check return value NULL, is it malloc failed?
+**			Our "pwd" takes 2 argument:
+**			1) int fd_stdout = filedescriptor to write standart output
+**			2) int fd_stderr = filedescriptor to write standart errors
 */
 
-/*
-**	Check EACCES errno flag! (man 3 getcwd)
-*/
-
-void		ft_pwd(int fd)
+void		ft_pwd(int fd_stdout, int fd_strerr)
 {
 	const char	*path_name = getcwd(NULL, 0);
+	char		*err;
 
 	if (!path_name)
-		ft_error(ALLOCATION_FAILED);
-	ft_putstr_fd((char *)path_name, fd);
-	ft_putstr_fd("\n", fd);
+	{
+		if (!(err = strerror(errno)))
+			ft_dprintf(fd_strerr, "ERRNO return NULL in ft_pwd ¯\\_(ツ)_/¯\n");
+		else
+			ft_dprintf(fd_strerr, "-csh: pwd: %s\n", err);
+	}
+	else
+		ft_dprintf(fd_stdout, "%s\n", (char *)path_name);
 	free((char *)path_name);
 }
